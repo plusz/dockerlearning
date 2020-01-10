e552294b2e779e63f69d918729da9b8e2b65d8d3
Piotr <plusz@users.noreply.github.com>
2020-01-10 17:17:07 +0100

    Add files via upload
    initial commit

---
+++ learning/docker/simpleweb/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hi Piotr');
+});
+
+app.listen(8080, () => {
+    console.log("Listening on port 8080");
+});
