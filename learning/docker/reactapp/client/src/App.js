e03de81517995f56ddff9fd527bcf521ac7f9b1c
plusz <Konto@Github410>
2020-01-13 23:08:55 +0100

    tests and nginx

--- learning/docker/reactapp/client/src/App.js
+++ learning/docker/reactapp/client/src/App.js
@@ -8,11 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world!
+          :) Hello world of Piotr? Hurrrah!
         </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
